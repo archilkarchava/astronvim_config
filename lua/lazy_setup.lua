4d7e8e2b1e1207dddb353e1d1128141162cac2dc
Archil Karchava <hello@archil.dev>
2024-09-08 17:06:42 +0400

    Limit the maximum amount of concurrent tasks
    Fixes the "Failed to spawn process git" error when syncing plugins

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
   install = { colorscheme = { "astrodark", "habamax" } },
+  concurrency = 10,
   ui = { backdrop = 100 },
   performance = {
     rtp = {
