a8dd70a2d620429294e80c99a88e435d420c9f2e
Archil Karchava <hello@archil.dev>
2024-09-08 15:43:07 +0400

    Add mini-splitjoin plugin

--- lua/community.lua
+++ lua/community.lua
@@ -53,6 +53,7 @@ return {
   { import = "astrocommunity.motion.mini-bracketed" },
   { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.motion.vim-matchup" },
+  { import = "astrocommunity.editing-support.mini-splitjoin" },
   { import = "astrocommunity.editing-support.dial-nvim" },
   { import = "astrocommunity.editing-support.undotree" },
   { import = "astrocommunity.editing-support.telescope-undo-nvim" },
