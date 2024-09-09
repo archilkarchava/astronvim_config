b58c338a82dcbbcf9882a4dd518093f2dcc5e08e
Archil Karchava <hello@archil.dev>
2024-09-10 01:01:19 +0400

    Use default exrc.nvim config

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -309,13 +309,7 @@ return {
         end,
       },
     },
-    opts = {
-      on_dir_changed = { -- Automatically load exrc files on DirChanged autocmd
-        enabled = true,
-        -- Wait until CursorHold and use vim.ui.select to confirm files to load, instead of loading unconditionally
-        use_ui_select = false,
-      },
-    },
+    opts = {},
   },
   {
     "gbprod/substitute.nvim",
