32513079a16a7f604544203dee45085d32caa73c
Archil Karchava <hello@archil.dev>
2024-09-11 02:49:17 +0400

    Refactor Neovide settings

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -2,33 +2,37 @@ if not vim.g.neovide then
   return {} -- do nothing if not in a Neovide session
 end
 
-vim.g.neovide_input_macos_option_key_is_meta = "only_left"
-
-local function paste() vim.api.nvim_paste(vim.fn.getreg "+", true, -1) end
-vim.keymap.set({ "n", "v", "s", "x", "o", "i", "l", "c", "t" }, "<D-v>", paste, { noremap = true, silent = true })
-vim.opt.termguicolors = true
-
+---@type LazySpec
 return {
   "AstroNvim/astrocore",
-  ---@type AstroCoreOpts
-  opts = {
-    options = {
-      opt = { -- configure vim.opt options
-        -- configure font
-        guifont = "BerkeleyMono Nerd Font:h15",
-        termguicolors = true,
-        -- line spacing
-        linespace = 0,
+  ---@param opts AstroCoreOpts
+  opts = function(_, opts)
+    local astrocore = require "astrocore"
+    local maps = assert(opts.mappings)
+    for _, mode in ipairs { "n", "v", "s", "x", "o", "i", "l", "c", "t" } do
+      maps[mode]["<D-v>"] = {
+        function() vim.api.nvim_paste(vim.fn.getreg "+", true, -1) end,
+        desc = "Paste from system clipboard",
+        silent = true,
+      }
+    end
+    return astrocore.extend_tbl(opts, {
+      mappings = maps,
+      options = {
+        opt = {
+          guifont = "BerkeleyMono Nerd Font:h15",
+          termguicolors = true,
+          linespace = 0,
+        },
+        g = {
+          neovide_input_macos_option_key_is_meta = "only_left",
+          neovide_scale_factor = 1.0,
+          neovide_padding_top = 0,
+          neovide_padding_bottom = 0,
+          neovide_padding_right = 0,
+          neovide_padding_left = 0,
+        },
       },
-      g = { -- configure vim.g variables
-        -- configure scaling
-        neovide_scale_factor = 1.0,
-        -- configure padding
-        neovide_padding_top = 0,
-        neovide_padding_bottom = 0,
-        neovide_padding_right = 0,
-        neovide_padding_left = 0,
-      },
-    },
-  },
+    })
+  end,
 }
