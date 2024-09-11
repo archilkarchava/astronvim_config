471b7f1db48cbb991ed665c804d106dca76cb52b
Archil Karchava <hello@archil.dev>
2024-09-11 20:03:44 +0400

    Increase lazy.nvim concurrency limit from 10 to 20

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -16,7 +16,7 @@ require("lazy").setup({
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
   install = { colorscheme = { "catppuccin", "astrodark", "habamax" } },
-  concurrency = 10,
+  concurrency = 20,
   ui = { backdrop = 100 },
   performance = {
     rtp = {
