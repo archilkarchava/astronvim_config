4caac1051ebdff8d2e27957b72020a80f2f7fd57
Archil Karchava <hello@archil.dev>
2024-09-19 13:22:35 +0400

    Fix input delay caused by vim-matchup

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -67,6 +67,7 @@ return {
           -- configure global vim variables (vim.g)
           -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
           -- This can be found in the `lua/lazy_setup.lua` file
+          matchup_matchparen_pumvisible = 1,
         },
       },
       -- Mappings can be configured through AstroCore as well.
