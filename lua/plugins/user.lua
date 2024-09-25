5f08f1afdf735bb38b08a539e2e4b22f4c4d9b7f
Archil Karchava <hello@archil.dev>
2024-09-25 23:16:06 +0400

    Reduce current_line_blame delay

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -298,7 +298,7 @@ return {
       current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-        delay = 300,
+        delay = 200,
         ignore_whitespace = false,
         virt_text_priority = 100,
         use_focus = true,
