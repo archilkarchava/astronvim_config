521cd0ee6b18ca7189149401dbc18e527be3530e
Archil Karchava <hello@archil.dev>
2024-10-12 12:40:05 +0400

    Disable neotest test discovery

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -8,7 +8,7 @@ return {
     version = false,
     opts = {
       discovery = {
-        enabled = true,
+        enabled = false,
         filter_dir = function(name) return name ~= "node_modules" end,
       },
     },
