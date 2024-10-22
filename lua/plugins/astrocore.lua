5ca64cfc886f35d7a52f75e94ee88adb211eb395
Archil Karchava <hello@archil.dev>
2024-10-22 18:08:05 +0400

    Add lazygit file history <leader>gh keymap

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -90,6 +90,16 @@ return {
       end
     end
 
+    if vim.fn.executable "git" == 1 and vim.fn.executable "lazygit" == 1 then
+      maps.n["<leader>gh"] = {
+        function()
+          local path = vim.fn.expand "%:p"
+          astrocore.toggle_term_cmd { cmd = "lazygit --filter " .. path, direction = "float" }
+        end,
+        desc = "Git commits (current file lazygit)",
+      }
+    end
+
     remap_key_if_exists(maps, "<C-PageUp>", "<C-Up>")
     remap_key_if_exists(maps, "<C-PageDown>", "<C-Down>")
     remap_key_if_exists(maps, "<C-Home>", "<C-Left>")
