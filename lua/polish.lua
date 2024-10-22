edc9d44a5824f8fa7a8718ba92bade6d042d9c97
Archil Karchava <hello@archil.dev>
2024-10-22 11:26:55 +0400

    Add commands to copy path to clipboard

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,18 +1,28 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
+vim.api.nvim_create_user_command("CopyPath", function()
+  local path = vim.fn.expand "%:p"
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard.')
+end, {})
+
+vim.api.nvim_create_user_command("CopyRelPath", function()
+  local path = vim.fn.expand "%:~:."
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard.')
+end, {})
+
 -- Set up custom filetypes
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
+-- vim.filetype.add {
+--   extension = {
+--     foo = "fooscript",
+--   },
+--   filename = {
+--     ["Foofile"] = "fooscript",
+--   },
+--   pattern = {
+--     ["~/%.config/foo/.*"] = "fooscript",
+--   },
+-- }
