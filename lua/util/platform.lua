108786ddfc5c1bc9624a392a5d18c07693a2652d
Archil Karchava <hello@archil.dev>
2024-10-30 21:39:11 +0400

    Add is_linux platform helper

--- lua/util/platform.lua
+++ lua/util/platform.lua
@@ -4,4 +4,6 @@ function M.is_macos() return vim.uv.os_uname().sysname == "Darwin" end
 
 function M.is_windows() return vim.uv.os_uname().sysname == "Windows_NT" end
 
+function M.is_linux() return vim.uv.os_uname().sysname == "Linux" end
+
 return M
