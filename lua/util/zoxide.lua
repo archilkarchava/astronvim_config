582c727cb905d26acdd024317481ea90601805a8
Archil Karchava <hello@archil.dev>
2024-11-06 03:43:03 +0400

    Small refactoring

--- lua/util/zoxide.lua
+++ lua/util/zoxide.lua
@@ -5,10 +5,16 @@ M.DATA_DIR_VAR_NAME = "_ZO_DATA_DIR"
 
 local zoxide_command_env = { [M.DATA_DIR_VAR_NAME] = M.DATA_DIR }
 
----@param dir string
-function M.remove(dir) return vim.system({ "zoxide", "remove", dir }, { env = zoxide_command_env }) end
+--- @param cmd (string[]) Zoxide command to execute
+local exec_zoxide = function(cmd)
+  local system_cmd = vim.list_extend({ "zoxide" }, cmd)
+  return vim.system(system_cmd, { env = zoxide_command_env })
+end
 
 ---@param dir string
-function M.add(dir) return vim.system({ "zoxide", "add", dir }, { env = zoxide_command_env }) end
+function M.remove(dir) return exec_zoxide { "remove", dir } end
+
+---@param dir string
+function M.add(dir) return exec_zoxide { "add", dir } end
 
 return M
