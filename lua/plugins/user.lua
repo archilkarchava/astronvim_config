8bf6b88ba11c40fd046fd18a68eead2788f44edf
Archil Karchava <hello@archil.dev>
2024-11-11 03:03:42 +0400

    Add useful globals for debugging

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1219,6 +1219,17 @@ return {
       statuscolumn = { enabled = false },
       words = { enabled = false },
     },
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VeryLazy",
+        callback = function()
+          -- Setup some globals for debugging (lazy-loaded)
+          _G.dd = function(...) Snacks.debug.inspect(...) end
+          _G.bt = function() Snacks.debug.backtrace() end
+          vim.print = _G.dd -- Override print to use snacks for `:=` command
+        end,
+      })
+    end,
   },
   {
     "mini.files",
