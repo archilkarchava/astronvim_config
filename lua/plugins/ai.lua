f48be321b78be22d0e56378ccc174adc17f561f2
Archil Karchava <hello@archil.dev>
2024-12-18 23:04:05 +0400

    feat(ai): use stable version of avante.nvim

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -305,6 +305,7 @@ return {
   },
   {
     "yetone/avante.nvim",
+    version = "*",
     event = "User AstroFile",
     build = is_windows and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
     opts = {
