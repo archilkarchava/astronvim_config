533d453c5132610a8bd9eb0a99f5cf63f23feaf3
Archil Karchava <hello@archil.dev>
2024-12-30 22:25:29 +0400

    chore(deps): use the latest version of avante.nvim

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -305,7 +305,6 @@ return {
   },
   {
     "yetone/avante.nvim",
-    version = "*",
     event = "User AstroFile",
     build = is_windows and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
     opts = {
