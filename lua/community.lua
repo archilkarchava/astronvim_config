9b7b904c6da06ba89570137a59c8d3733f44ac3d
Archil Karchava <hello@archil.dev>
2025-01-10 20:57:13 +0400

    feat(tasks): add overseer.nvim plugin

--- lua/community.lua
+++ lua/community.lua
@@ -12,6 +12,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
 
+  { import = "astrocommunity.code-runner.overseer-nvim" },
   { import = "astrocommunity.color.nvim-highlight-colors" },
   -- { import = "astrocommunity.colorscheme.vscode-nvim" },
   -- { import = "astrocommunity.colorscheme.github-nvim-theme" },
