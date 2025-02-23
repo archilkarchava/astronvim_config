980045c753e55c67c96f4d694ebc44853a27ebff
Archil Karchava <hello@archil.dev>
2025-02-23 13:47:13 +0400

    chore(test): disable overseer consumer by default

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -49,6 +49,11 @@ return {
           consumers = {
             overseer = require "neotest.consumers.overseer",
           },
+          overseer = {
+            enabled = true,
+            -- When this is true (the default), it will replace all neotest.run.* commands
+            force_default = false,
+          },
         },
       },
     },
