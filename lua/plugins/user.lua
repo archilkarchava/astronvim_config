1ac83c88e3a5025a53d066cacc7de135ae0dc6b1
Archil Karchava <hello@archil.dev>
2025-03-02 00:42:38 +0400

    feat(snippets): import snippets from VS Code

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1956,6 +1956,19 @@ return {
     end,
   },
   {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      -- add more custom luasnip configuration such as filetype extend or custom snippets
+      local luasnip = require "luasnip"
+      luasnip.filetype_extend("javascript", { "javascriptreact" })
+      luasnip.filetype_extend("typescript", { "typescriptreact" })
+      require("luasnip.loaders.from_vscode").lazy_load {
+        paths = { vim.fn.stdpath "config" .. "/snippets" },
+      }
+    end,
+  },
+  {
     "better-escape.nvim",
     enabled = false,
   },
