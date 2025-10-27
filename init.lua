33a983c825a355cca1646422584bf8f10f3a2b00
Archil Karchava <hello@archil.dev>
2025-10-27 10:51:15 +0400

    Revert "fix: Use lazy.nvim fork to fix nvim-neorocks issue"
    This reverts commit 543229220f8531f299f08b61f3ae6024a94d01f7.

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/mats852/lazy.nvim.git", "--branch=bugfix/nvim-neorocks-to-lumen-oss", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
